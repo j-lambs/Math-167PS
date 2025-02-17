46b38f7759637dd0898f6141e3c3c72f12cb3695
j-lambs <jacklol87@yahoo.com>
2025-02-17 02:05:30 -0800

    Completely cooked TicTacToe v1

--- temp.py
+++ temp.py
@@ -1,3 +1,17 @@
-name = 'NTOANTA'
-name = name.lower()
-print(name)
+def print_current_board(board):
+    for row in board:
+        print(row)
+
+def vert(a):
+    for j in range(len(a)):
+        for i in range(len(a)):
+            print(a[i][j])
+
+a = [[1,2,3],[4,5,6],[7,8,9]]
+
+x = 0
+typeofx = type(x)
+if isinstance(x, int):
+    print('hi')
+if isinstance(x, str):
+    print('no')
